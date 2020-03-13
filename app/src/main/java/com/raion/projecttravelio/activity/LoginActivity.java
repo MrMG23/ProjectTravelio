459952ec1299be2199b55f2da54c2eb89023a022
MrMG23 <naufalrafino23@gmail.com>
2020-03-13 16:55:40 +0700

    icon TRAVELUR bckgr putih

--- app/src/main/java/com/raion/projecttravelio/activity/LoginActivity.java
+++ app/src/main/java/com/raion/projecttravelio/activity/LoginActivity.java
@@ -3,14 +3,18 @@ package com.raion.projecttravelio.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.raion.projecttravelio.R;
 
 public class LoginActivity extends AppCompatActivity {
+    private TextView appName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        appName = findViewById(R.id.app_name);
+
     }
 }
