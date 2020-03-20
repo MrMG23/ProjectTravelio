b76da98ff2f2f1b4833739fc1a3acc787be54abf
MrMG23 <naufalrafino23@gmail.com>
2020-03-20 17:41:05 +0700

    login otw, semua activity

--- app/src/main/java/com/raion/projecttravelio/activity/LoginActivity.java
+++ app/src/main/java/com/raion/projecttravelio/activity/LoginActivity.java
@@ -1,32 +1,97 @@
 package com.raion.projecttravelio.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.raion.projecttravelio.MainActivity;
 import com.raion.projecttravelio.R;
+
+
 
 public class LoginActivity extends AppCompatActivity {
     private TextView appName;
     private EditText email;
     private EditText password;
+    private Button btnLogin;
+    private DatabaseReference databaseReference;
+    private String ID_KEY = "idkey";
+    private String id_key = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    /*    appName = findViewById(R.id.app_name);
+        btnLogin = findViewById(R.id.btn_login);
         email = findViewById(R.id.emailL_login);
-        password = findViewById(R.id.pass_login);*/
-        Intent intent = getIntent();
+        password = findViewById(R.id.pass_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String fEmail = email.getText().toString();
+                final String fPaassword = password.getText().toString();
+                if(fEmail.isEmpty()){
+                    Toast.makeText(getApplicationContext(), "Email Kosong!", Toast.LENGTH_SHORT).show();
+                }else{
+                    if (fPaassword.isEmpty()){
+                        Toast.makeText(getApplicationContext(), "Password Anda Kosong!", Toast.LENGTH_SHORT).show();
+                    }else{
+                        databaseReference = FirebaseDatabase.getInstance().getReference().child("Akun");
+                        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                for (int i = 1; i<= dataSnapshot.getChildrenCount(); i++){
+                                    if (dataSnapshot.child(String.valueOf(i)).child("email").getValue().toString().equals(fEmail)) {
+                                        String passwordFromFirebase = dataSnapshot.child(String.valueOf(i)).child("pass").getValue().toString();
+                                        if (fPaassword.equals(passwordFromFirebase)) {
+                                            //Simpan username (key) kepada local
+                                            SharedPreferences sharedPreferences = getSharedPreferences(ID_KEY, MODE_PRIVATE);
+                                            SharedPreferences.Editor editor = sharedPreferences.edit();
+                                            editor.putString(id_key, String.valueOf(i));
+                                            editor.apply();
+
+                                            Intent goToHome = new Intent(LoginActivity.this, MainActivity.class);
+                                            startActivity(goToHome);
+                                        } else {
+                                            Toast.makeText(getApplicationContext(), "Password salah!", Toast.LENGTH_SHORT).show();
+                                        }
+                                    }else
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                            }
+                        });
+                    }
+                }
+            }
+        });
     }
 
     public void signUpActivity(View view){
         Intent intent = new Intent(this, SignupActivity.class);
         startActivity(intent);
     }
+
+//    if (password.equals(passwordFromFirebase)) {
+//        //Simpan username (key) kepada local
+//        SharedPreferences sharedPreferences = getSharedPreferences(DRIVERID_KEY, MODE_PRIVATE);
+//        SharedPreferences.Editor editor = sharedPreferences.edit();
+//        editor.putString(driverid_key, String.valueOf(i));
+//        editor.apply();
 }
